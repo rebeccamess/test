2dcae09e19f46ab6b3672b380036085c167b5d0e
Rebecca Mes <r.mes@student.tudelft.nl>
2023-02-23 13:38:51 +0100

    Added main.py and a function for addition

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 print(f'1 + 3 = {1+3}')
 
-#!/user.bin/env python 3
+#!/user.bin/env python3
 
 def add(left_hand, right_hand):
     return left_hand + right_hand
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/user.bin/env python3
+
+from calc import add
+
+def main():
+    print(add(1, 3))
+
+if __name__ == "__main__":
+    print(main())
+
